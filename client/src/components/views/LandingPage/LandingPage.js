61bbd3cb985e81a0e4e9efa2f5e0aaba8397df66
shirlyfang <shirly.is.here@gmail.com>
2020-08-20 00:17:35 -0700

    Added formatting and styles for homepage plus poppins

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { FaCode } from "react-icons/fa";
+import './styles.css'
+import '../../../index.css'
 
 function LandingPage() {
+    var staticWelcome = "Event description and welcome message: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus finibus mauris. Pellentesque tempor lacus sit amet consectetur malesuada."
+    var staticDiscord = "Join us on Discord!"
+    var discordLink = "https://discord.gg/rKwaCr"
     return (
         <>
             <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+                <img className="image" src={require('./assets/logo.png')} />
+                <div className="middle-container">
+                    <span className="middle-text">{staticWelcome}</span>
+                </div>
+                <div className="middle-container">
+                    <span className="middle-text">{staticDiscord}</span>
+                    <a className="middle-text" href={discordLink} target="_blank">
+                        {discordLink}
+                    </a>
+                </div>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
         </>
     )
 }
