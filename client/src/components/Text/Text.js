7de32bda9fe06546a31bb62e7c02b3ede7c755a6
Kristie Lim <kristie.lim@gmail.com>
2020-08-30 13:36:32 -0700

    Change font to constant

--- client/src/components/Text/Text.js
+++ client/src/components/Text/Text.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { css } from "emotion";
 import { Colors } from "../../constants/Colors";
+import * as Fonts from "../../constants/Fonts";
 
 function Text({
-  children,
-  color = Colors.Black,
-  size = "18px",
-  weight = 400,
-  style,
+	children,
+	color = Colors.Black,
+	size = "18px",
+	weight = 400,
+	style,
 }) {
-  return (
-    <div
-      className={css`
-        color: ${color};
-        font-size: ${size};
-        font-weight: ${weight};
-        font-family: Poppins;
-      `}
-      style={style}
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			className={css`
+				color: ${color};
+				font-size: ${size};
+				font-weight: ${weight};
+				font-family: ${Fonts.Primary};
+			`}
+			style={style}
+		>
+			{children}
+		</div>
+	);
 }
 
 export default Text;
--- client/src/components/TextInput/TextInput.js
+++ client/src/components/TextInput/TextInput.js
@@ -1,32 +1,33 @@
 import React from "react";
 import { css } from "emotion";
 import { Colors } from "../../constants/Colors";
+import * as Fonts from "../../constants/Fonts";
 
 function TextInput({
-  value,
-  onChange,
-  border = `2px solid ${Colors.Blue}`,
-  height = "42px",
-  width = "300px",
-  style,
+	value,
+	onChange,
+	border = `2px solid ${Colors.Blue}`,
+	height = "42px",
+	width = "300px",
+	style,
 }) {
-  return (
-    <input
-      className={css`
-        border: ${border};
-        height: ${height};
-        width: ${width};
-        border-radius: 16px;
-        padding: 12px;
-        font-family: Poppins;
-        outline: none;
-      `}
-      style={style}
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-  );
+	return (
+		<input
+			className={css`
+				border: ${border};
+				height: ${height};
+				width: ${width};
+				border-radius: 16px;
+				padding: 12px;
+				font-family: ${Fonts.Primary};
+				outline: none;
+			`}
+			style={style}
+			type="text"
+			value={value}
+			onChange={onChange}
+		/>
+	);
 }
 
 export default TextInput;
