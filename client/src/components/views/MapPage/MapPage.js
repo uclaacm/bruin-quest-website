f1010872b8bacbbf99308e9d95bf15d1218b9ec1
Henry Chang <hchang23@g.ucla.edu>
2020-09-02 17:34:09 -0500

    updated map page

--- client/src/components/views/MapPage/MapPage.js
+++ client/src/components/views/MapPage/MapPage.js
@@ -9,27 +9,14 @@ const MapPage = () => {
   const [area, setArea] = useState("")
 
   return (
-    <>
       <div className="app">
         <div className="map-view">
-          <div className="map-list">
-            <ol>
-            <p>Selected Area: {area}</p>
-              <li>Royce Hall</li>
-              <li>Powell</li>
-              <li>Long Bricky Stairs</li>
-              <li>Watery Fountain</li>
-              <li>Rectangle Bar</li>
-            </ol>
-          </div>
           
           <UCLAMap setArea={setArea}/>
       </div>
       </div>
-      <div style={{ float: "right" }}>
-        Thanks For Using This Boiler Plate by John Ahn
-			</div>
-		</>
+
+
 	);
 }
 
