bfaed3df50269fce7dbb2be97bbc6824bb9d7084
Kristie Lim <kristie.lim@gmail.com>
2020-09-12 20:29:01 -0700

    Use async/await

--- server/routes/puzzle.js
+++ server/routes/puzzle.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Puzzle } = require('../models/Puzzle');
+const { Puzzle } = require("../models/Puzzle");
 
-// const { auth } = require('../middleware/auth');
+const { auth } = require("../middleware/auth");
 
-// router.get("/:id", auth, (req, res) => {
-router.get('/:id', (req, res) => {
-	Puzzle.findById(req.params.id, (err, puzzle) => {
-		if (err) {
-			res.status(500).json({ success: false });
-		}
-		delete puzzle.correctAnswer;
-		// console.log(req.user._id);
+router.get("/:id", auth, async (req, res) => {
+	try {
+		const puzzle = await Puzzle.findById(req.params.id);
+		const teamAnswers = await Team.findById();
+		console.log(req.team._id);
 		res.status(200).json(puzzle);
-	});
+	} catch (error) {
+		res.status(500).json({ success: false, error });
+	}
 });
 
 module.exports = router;
