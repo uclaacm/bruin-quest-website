767e6a5a94863ebf3a8c4f3b1d5fcc9970bddf34
shirlyfang <shirly.is.here@gmail.com>
2020-09-22 23:00:52 -0700

    Updated design for landing and scoreboard (#46)
    * Updated design for landing and scoreboard

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,60 +3,80 @@ import { css } from 'emotion';
 import * as Colors from '../../../constants/Colors';
 import * as Fonts from '../../../constants/Fonts';
 import Text from '../../Text/Text';
+import Button from '../../Button/Button';
 import logo from './assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const staticWelcome = `Event description and welcome message: Lorem ipsum 
 dolor sit amet, consectetur adipiscing elit. Mauris faucibus finibus 
 mauris. Pellentesque tempor lacus sit amet consectetur malesuada.`;
-const staticDiscord = 'Join us on Discord!';
+const staticTitle = 'Welcome Traveler!';
 const discordLink = 'https://discord.gg/rKwaCr';
-
-const textStyle = css`
-  text-align: center;
-  font-size: 2.5rem;
-  font-family: ${Fonts.Primary}; 
-`;
-
-const middleContainer = css`
-  flex-direction: column;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 45vw;
-`;
 
 export default function LandingPage() {
 	return (
 		<main
 			className={css`
-        flex-direction: column;
         display: flex;
         justify-content: space-evenly;
         align-items: center;
         height: 100vh;
       `}
 		>
-			<img className={css` width: 40vw`} src={logo} alt=""/>
-			<div className={middleContainer}>
-				<Text className={textStyle}>
+			<img className={css` height: 80vh`} src={logo} alt="Bruin Quest Logo"/>
+			<div
+				className={css`
+          flex-direction: column;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 45vw;
+        `}
+			>
+				<Text
+					className={css`
+            text-align: left;
+            font-size: 7rem;
+            font-family: ${Fonts.Primary}; 
+            font-weight: 600;
+            font-color: ${Colors.Black};
+          `}
+				>
+					{staticTitle}
+				</Text>
+				<Text
+					className={css`
+            text-align: left;
+            font-size: 2rem;
+            font-family: ${Fonts.Primary};
+          `}
+				>
 					{staticWelcome}
 				</Text>
-			</div>
-			<div className={middleContainer}>
-				<Text className={textStyle}>
-					{staticDiscord}
-				</Text>
-				<a
+				<div
 					className={css`
-            color: ${Colors.Blue};
-            font-size: 2rem;
-            text-decoration: underline;
+            display: flex;
+            justify-content: flex-start;
+            width: 100%;
+            padding-top: 50px;
           `}
-					href={discordLink}
-					rel="noopener noreferrer"
-					target="_blank">
-					{discordLink}
-				</a>
+				>
+					<Link
+						to="/register" className={css`padding-right: 30px;`}
+					>
+						<Button color={Colors.MediumBlue} height='50px' width='200px'>
+            Begin Quest
+						</Button>
+					</Link>
+					<Button
+						color={Colors.Gray}
+						height='50px'
+						width='300px'
+						onClick={() => window.open(discordLink, '_blank')}
+					>
+            Join the Discord
+					</Button>
+				</div>
 			</div>
 		</main>
 	);
