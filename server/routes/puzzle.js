05c50e962034933a2d1246627c7fb9de2894a411
Kristie Lim <kristie.lim@gmail.com>
2020-09-30 16:22:32 -0700

    Get puzzle with team answer

--- server/routes/puzzle.js
+++ server/routes/puzzle.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { Puzzle } = require("../models/Puzzle");
+const { Team } = require("../models/Team");
 
 const { auth } = require("../middleware/auth");
 
 router.get("/:id", auth, async (req, res) => {
 	try {
 		const puzzle = await Puzzle.findById(req.params.id);
-		const teamAnswers = await Team.findById();
-		console.log(req.team._id);
-		res.status(200).json(puzzle);
+		const teamAnswers = await Team.findById(req.team._id, "puzzles");
+		delete puzzle.correctAnswer;
+		const teamAnswer = teamAnswers.filter(
+			(teamAnswer) => teamAnswer.id === req.params.id
+		);
+		console.log({ ...puzzle, ...teamAnswer });
+		res.status(200).json({ ...puzzle, ...teamAnswer });
 	} catch (error) {
 		res.status(500).json({ success: false, error });
 	}
