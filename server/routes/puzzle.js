85b2b9e2a97decb34aebd120a7ccc6ee8dc1e430
Kristie Lim <kristie.lim@gmail.com>
2020-09-30 20:49:03 -0700

    Changed object to document

--- server/routes/puzzle.js
+++ server/routes/puzzle.js
@@ -1,22 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Puzzle } = require("../models/Puzzle");
-const { Team } = require("../models/Team");
+const { Puzzle } = require('../models/Puzzle');
+const { Team } = require('../models/Team');
 
-const { auth } = require("../middleware/auth");
+const { auth } = require('../middleware/auth');
 
-router.get("/:id", auth, async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
 	try {
-		const puzzle = await Puzzle.findById(req.params.id);
-		const teamAnswers = await Team.findById(req.team._id, "puzzles");
+		const puzzle = (await Puzzle.findById(req.params.id).exec()).toObject();
+		const teamAnswers = (
+			await Team.findById(req.team._id, 'puzzles').exec()
+		).toObject();
 		delete puzzle.correctAnswer;
-		const teamAnswer = teamAnswers.filter(
-			(teamAnswer) => teamAnswer.id === req.params.id
+		const teamAnswer = teamAnswers.puzzles.find(
+			teamAnswer => teamAnswer._id === req.params.id
 		);
-		console.log({ ...puzzle, ...teamAnswer });
 		res.status(200).json({ ...puzzle, ...teamAnswer });
 	} catch (error) {
-		res.status(500).json({ success: false, error });
+		res.status(500).json({ success: false, error: error.message });
 	}
 });
 
