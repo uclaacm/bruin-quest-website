34386a91d2ad553f0e217c0c079f434b548454c9
Alvin Nguyen <alvinonguyen00@gmail.com>
2020-10-06 12:51:03 -0700

    Added mongo-sanitizer to general area route

--- server/routes/general-areas.js
+++ server/routes/general-areas.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const sanitize = require('mongo-sanitize');
 const router = express.Router();
 const { GeneralArea } = require('../models/GeneralArea');
 
 router.get('/:name', (req, res) => {
-	GeneralArea.findOne({ name: req.params.name }).exec()
+	GeneralArea.findOne({ name: sanitize(req.params).name }).exec()
 		.then(area => {
 			if (area) {
 				res.send(area);
