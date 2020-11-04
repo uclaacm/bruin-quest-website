762f2bb3dda85872d50c1471e7c6eda39eeae0bc
Jody Lin <jody.m.lin@gmail.com>
2020-11-03 21:37:40 -0800

    finished 404 page

--- client/src/components/views/NotFound/NotFound.js
+++ client/src/components/views/NotFound/NotFound.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Typography, Row, Col } from 'antd';
 import { css } from 'emotion';
 
 import Text from '../../Text/Text';
@@ -12,36 +11,39 @@ import * as Colors from '../../../constants/Colors';
 import * as Fonts from '../../../constants/Fonts';
 import Button from '../../../components/Button/Button.js';
 
-const { Title } = Typography;
-
 const classes = {
 	page: css`
-		padding: 2vw;
+		padding: 16px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+	`,
+	container: css`
+		max-width: 700px;
 	`,
 	title: css`
-		text-align: left;
-		font-size: 6rem;
-		font-family: ${Fonts.Primary}; 
+		text-align: center;
+		font-size: 3rem;
+		font-family: ${Fonts.Primary};
 		font-weight: 600;
 		font-color: ${Colors.Black};
 		padding-bottom: 16px;
 	`,
 	bodyText: css`
-		text-align: left;
-		font-family: ${Fonts.Primary}; 
+		text-align: center;
+		font-family: ${Fonts.Primary};
 		font-color: ${Colors.Black};
-		font-size: 2rem;
-	`,
-	bodyContainer: css`
-		margin-bottom: 16px;
+		font-size: 1.5rem;
 	`,
 	imageContainer: css`
 		display: flex;
 		justify-content: center;
-		margin-bottom: 16px;
+		margin: 16px 0;
 	`,
 	locationImage: css`
-		width: 500px;
+		max-width: 80%;
+		border-radius: 16px;
 	`
 };
 
@@ -62,7 +64,6 @@ const locations = [
 		name: 'Santa Monica Beach',
 		image: SMBeach
 	}
-
 ];
 
 function RandomLocation() {
@@ -73,32 +74,31 @@ function RandomLocation() {
 function LoginPage(props) {
 	const location = RandomLocation();
 	return (
-		<div className={classes.page}>
-			<Text
-				className={classes.title}
-			>
-        Not Found
-			</Text>
-			<Row className={classes.imageContainer}>
-				<Col span={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
-					<div className={classes.bodyContainer}>
-						<Text className={classes.bodyText}>
-							It seems you are lost and found yourself at...
-						</Text>
-					</div>
-					<Text className={classes.bodyText}>
-						<b>{location.name}</b>
-					</Text>
-				</Col>
-				<Col span={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
+		<div id='page' className={classes.page}>
+			<div id='container' className={classes.container}>
+				<Text
+					className={classes.title}
+				>
+					Not Found
+				</Text>
+				<Text className={classes.bodyText}>
+					It seems you are lost and found yourself at...
+				</Text>
+				<Text className={classes.bodyText}>
+					<b>{location.name}</b>
+				</Text>
+				<div className={classes.imageContainer}>
 					<img className={classes.locationImage} src={location.image}/>
-				</Col>
-			</Row>
-			<Button onClick={() => {
-				window.location.href = '/';
-			}}>
-				Back Home
-			</Button>
+				</div>
+				<Button
+					style={{ margin: 'auto' }}
+					onClick={() => {
+						window.location.href = '/';
+					}}
+				>
+					Back Home
+				</Button>
+			</div>
 		</div>
 	);
 }
